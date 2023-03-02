d76115a52863a29dfad80abd41da2c92ba7a39cc
klee2024 <klee2024@uchicago.edu>
2023-03-02 19:49:59 +0000

    added associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,9 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to :director
+  has_many :characters
 
+  has_many: cast, :through => :characters, :source => :actor
   
 end
